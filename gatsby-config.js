7bf40769a25843ed616993d43ef65849e9e81d65
Simona Cotin <simona.cotin@microsoft.com>
2021-11-26 17:24:48 +0100

    make change

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteMetadata: {
-    title: "Staging Angular",
+    title: "Staging & PR Angular",
     subtitle: "One framework.  Mobile & desktop.",
     description:
       "Introduction to Angular workshop - presented by Simona Cotin and Artur Daschevici. Thanks Brian Holt (@holtbt) for the awesome gatsby course template.",
